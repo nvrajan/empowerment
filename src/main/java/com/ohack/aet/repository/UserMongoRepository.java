28d85a5da37c59794f813c08fdb5c6c2dce6fd32
Basha <arbasha@paypalcorp.com>
2016-11-12 16:03:54 +0530

    Added User Mongo Repo

---
+++ src/main/java/com/ohack/aet/repository/UserMongoRepository.java
@@ -0,0 +1,7 @@
+package com.ohack.aet.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ohack.aet.model.User;
+
+public interface UserMongoRepository extends CrudRepository<User, String>{}
